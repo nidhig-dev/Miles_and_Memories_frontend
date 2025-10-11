b33c21318943d3c252c239f4067079787212c0ff
nidhig-dev <goyal.nidhi@gmail.com>
2025-10-10 20:34:35 -0400

    Added pages and tailwind

---
+++ README.md
@@ -0,0 +1,6 @@
+# React + Vite
+
+npm create vite@latest .
+npm i react-router-dom
+npm install tailwindcss @tailwindcss/vite    
+                                                  
---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import './App.css'
+import { Routes, Route } from 'react-router-dom';
+
+//import pages
+import Login from "./pages/Auth/Login";
+import Signup from "./pages/Auth/Signup";
+import Home from "./pages/Home/Home";
+import Missing from "./pages/Missing";
+
+//import components
+
+
+function App() {
+
+  return (
+    <>
+      <Routes>
+        <Route path="/dashboard" element={<Home />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<Missing />} />
+      </Routes>
+
+
+    </>
+  )
+}
+
+export default App
