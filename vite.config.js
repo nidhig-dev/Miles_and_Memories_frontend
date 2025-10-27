f5cc416cdb613e8f4a33993674e4a8c547dac271
nidhig-dev <goyal.nidhi@gmail.com>
2025-10-27 13:10:33 -0400

    deploying...

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  root: '.',   // force root at repo root
   base: '/',
-  plugins: [react()
-  ],
-
+  plugins: [react()],
 })
